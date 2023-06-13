8b884260259e9ea4878ab88c145c7abef13354f5
Lorena Zuba <Loreninhazuba22@gmail.com>
2023-06-13 19:49:48 -0300

    Base da pagina

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unidades Federativas brasileiras</title>
+</head>
+<body>
+    <header>
+        <h2>Estados do Brasil</h2>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <p>Lorena Zuba - &copy 2023 </p>
+    </footer>
+    
+</body>
+</html>
