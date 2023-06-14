0383021553a184dcd82fb268a3735814f02f406e
LorenaZuba <loreninhazuba22@gmail.com>
2023-06-13 21:53:46 -0300

    Estilos

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Chau+Philomene+One&family=Lato:wght@300&family=Raleway&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap" rel="stylesheet">
     <title>Unidades Federativas brasileiras</title>
 </head>
 <body>
@@ -31,7 +36,6 @@
                     'expvida' => '73,9 anos',
                 )
             ];
-            echo var_dump($Acre);
         ?>
 
         <table>
